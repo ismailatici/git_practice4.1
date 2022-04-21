32a9415cebb76ea0428b426414b3cc6f32eff9e0
ismailatici <ismailatici@aol.com>
2022-04-22 00:41:34 +0200

    doing final work on hello  feature

--- src/Hello/Hello.java
+++ src/Hello/Hello.java
@@ -3,6 +3,7 @@ package Hello;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("eu8 learning....");
     }
 
 }
